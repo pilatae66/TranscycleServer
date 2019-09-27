973b4f5f7a198920f941458a1087e79d23054484
pilatae66 <pilatae66@gmail.com>
2019-09-27 21:47:01 +0800

    Fix

--- resources/js/plugins/store.js
+++ resources/js/plugins/store.js
@@ -5,7 +5,9 @@ import Vuex from 'vuex'
 import router from '../routes/routes'
 
 Vue.use(Vuex)
-let url = process.env.APP_ENV !== 'production' ? 'http://localhost:8000' : 'http://mighty-savannah-84780.herokuapp.com'
+let localURL = 'http://localhost:8000'
+let serverURL = 'http://mighty-savannah-84780.herokuapp.com'
+let url = serverURL
 
 
 export default new Vuex.Store({
